c40d2daab0a9a6f5f55ecf8f924c8cedbbdc01cf
Daniel Felleman <daniel.felleman@revature.com>
2022-10-27 12:45:17 -0400

    adding output files for logs and errors

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-BUILD_ID=dontKillMe nohup java -jar ./target/HelloECTwo-1.0-SNAPSHOT-jar-with-dependencies.jar &
+BUILD_ID=dontKillMe nohup java -jar ./target/HelloECTwo-1.0-SNAPSHOT-jar-with-dependencies.jar 1>logs.out 2>error.out &
