1fa78b462849502fa4946217353c4c2a162d624d
Daniel Felleman <daniel.felleman@revature.com>
2022-10-27 15:52:05 -0400

    reverting back to sudo kill

--- startup.sh
+++ startup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 whoami
-sudo su
 whoami
 PID=$(pgrep -f Hello | awk '{print $1}')
 if [[ "" != "$PID" ]]; then
   echo "killing $PID"
-  kill $PID
+  sudo kill $PID
 fi
 echo "starting jar"
 nohup java -jar ./target/HelloECTwo-1.0-SNAPSHOT-jar-with-dependencies.jar > nohup.out &
