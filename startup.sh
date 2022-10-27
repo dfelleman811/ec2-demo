c6750aad4588fee1b283c2132b25aa5523c72916
Daniel Felleman <daniel.felleman@revature.com>
2022-10-27 13:11:11 -0400

    adding conditional check to kill running process if exists

--- startup.sh
+++ startup.sh
@@ -1,1 +1,6 @@
+PID=$(pgrep -an java | awk '{print $1}')
+if [ "" != "$PID" ]; then
+  echo "killing $PID"
+  kill $PID
+fi
 nohup java -jar ./target/HelloECTwo-1.0-SNAPSHOT-jar-with-dependencies.jar 1>logs.out 2>error.out &
